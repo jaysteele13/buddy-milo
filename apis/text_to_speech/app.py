9a32cc76e0a4772474f4c8b0cac36aaa864fd8f7
jay <jay.steele2003@gmail.com>
2025-05-19 00:29:36 +0100

    api-jargon

---
+++ apis/text_to_speech/app.py
@@ -0,0 +1,7 @@
+# Pseduo Code
+
+# Configure Restful API using FastAPI with correct naming conventions -> save to README
+
+# import whisper model locally (probably tiny) await the model prediction when code is brought in
+
+# output the model output, have validation to give error if audio file is bad or incorrect
